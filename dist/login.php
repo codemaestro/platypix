6c096c83e4f83b0ed32860707dafbb10ac896c2c
Tony Chung <tonyc@codemaestro.com>
2025-06-07 23:03:54 -0400

    Update auth

--- dist/login.php
+++ dist/login.php
@@ -18,14 +18,30 @@ include_once '../lib/auth.php';
       <p class="error"><?php echo htmlspecialchars($error); ?></p>
     <?php endif; ?>
     <form method="post" action="">
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required />
+      <p><label for="password">Password:</label>
+        <input type="password" id="password" name="password" required /><br>
+        <a href="#" id="toggle-password">Show password</a>
+      </p>
       <button type="submit">Login</button>
     </form>
   </div>
   <script>
     // Focus the password input field when the page loads
     document.getElementById('password').focus();
+    // Toggle password visibility
+    document.getElementById('toggle-password').addEventListener('click', function(e) {
+      e.preventDefault();
+      const pwd = document.getElementById('password');
+      if (pwd.type === 'password') {
+        this.classList.add('active');
+        pwd.type = 'text';
+        this.textContent = 'Hide password';
+      } else {
+        this.classList.remove('active');
+        pwd.type = 'password';
+        this.textContent = 'Show password';
+      }
+    });
   </script>
   <?php include_once '../lib/footer.php'; ?>
 </body>
--- lib/auth.php
+++ lib/auth.php
@@ -3,6 +3,8 @@
 $env = parse_ini_file('../.env');
 define('ACCESSLOG', $env['ACCESSLOG']);
 define('ACCESSPASS', $env['ACCESSPASS']);
+define('COOKIESET', $env['COOKIESET']);
+define('COOKIEVAL', $env['COOKIEVAL']);
 
 // define logging function
 function log_msg($message)
@@ -12,14 +14,19 @@ function log_msg($message)
 
 // check if the user is logged in
 session_start();
+if (isset($_SERVER["DOCUMENT_ROOT"]) && empty($_SERVER["DOCUMENT_ROOT"])) {
+  $_COOKIE[COOKIESET] = COOKIEVAL; // Simulate a local environment for testing
+  $_SESSION['logged_in'] = true;
+}
+
 if (
   !isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true ||
-  !isset($_COOKIE['auth']) || $_COOKIE['auth'] !== '1'
+  !isset($_COOKIE[COOKIESET]) || $_COOKIE[COOKIESET] !== COOKIEVAL
 ) {
   // If form submitted, check password
   if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['password']) && $_POST['password'] === ACCESSPASS) {
-      setcookie('auth', '1', time() + 60 * 60 * 24 * 7, '/');
+      setcookie(COOKIESET, COOKIEVAL, time() + 60 * 60 * 24 * 7, '/');
       $_SESSION['logged_in'] = true;
       header('Location: /');
       exit;
@@ -38,9 +45,9 @@ if (
 
 // Log the access
 $date = date('m/d/Y h:i:s a', time());
-$request = sprintf("%s%s",
+$request = sprintf(
+  "%s%s",
   isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'DOMAIN.COM',
   isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : 'REQUEST_URI'
 );
 log_msg("{$request} accessed at {$date}");
-?>
